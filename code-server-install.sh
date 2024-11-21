78217e4265bf942ce62007b0aa0a8b4085329fb3
Marcos V. Theodoro <marcosvinytheo@gmail.com>
2024-11-21 16:30:43 -0300

    [Feat]: update code-server installation shell script for Fedora

--- code-server-install.sh
+++ code-server-install.sh
@@ -2,4 +2,6 @@
 
 sudo apt install curl
 
-curl -fsSL https://code-server.dev/install.sh | sh
+curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep "browser_download_url.*amd64.rpm" | cut -d '"' -f 4 | xargs curl -LO
+
+sudo rpm -i code-server-*-amd64.rpm
