bb1c11fe18248dc4001c002284b1977dbf003a3b
Marcos Vinícius Theodoro Pinheiro da Silva <marcos_theodoro.20221+github@poli.ufrj.br>
2025-03-14 21:34:13 -0300

    [Feat]: update code-server installation shell script for Debia based distros

--- code-server-install.sh
+++ code-server-install.sh
@@ -2,6 +2,6 @@
 
 sudo apt install curl
 
-curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep "browser_download_url.*amd64.rpm" | cut -d '"' -f 4 | xargs curl -LO
+curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep "browser_download_url.*amd64.deb" | cut -d '"' -f 4 | xargs curl -LO
 
-sudo rpm -i code-server-*-amd64.rpm
+sudo dpkg -i code-server-*-amd64.deb
